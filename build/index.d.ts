f36572ea6516f5c1a73a123fa577ea24d226b92b
Bevan Wentzel <bevan@teamgeek.co.za>
2020-06-22 10:05:41 +0200

    Updated build

--- build/index.d.ts
+++ build/index.d.ts
@@ -53,6 +53,7 @@ interface Props {
     sortVariable?: string;
     initialSort?: string;
     multiselect?: boolean;
+    actions?: Action[];
     onSuccess?: (result: string) => void;
     onWarning?: () => void;
     onError?(message: string): void;
